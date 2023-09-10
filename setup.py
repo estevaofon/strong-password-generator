9211ddc8b97626450a345b79034ccf687dd17a36
Estevao <estevaopfon@gmail.com>
2023-09-10 00:09:07 -0300

    🐛 fix(setup.py): change console script name from 'strong_password' to 'strong-password'

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'strong_password = strong_password:main',
+            'strong-password = strong_password:main',
         ],
     },
 )
